8dad8ae0d177e5ecb092fce7d75b23607b6cfd2e
tmurray19 <t.murray19@nuigalway.ie>
2018-11-08 09:47:49 +0000

    Resolve

--- chefskitchen/chefdecuisine/urls.py
+++ chefskitchen/chefdecuisine/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('chef/id/<int:souschef_id>', views.souschef),
+        path('chef/id/<int:souschef_id>', views.souschef, name='souschef_profile'),
         path('chefs/', views.souschef_list),
        
 ]
